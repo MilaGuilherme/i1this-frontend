3514ddf5ec2319d0a2dfdb87b1738a6adf276a50
Mila <mila.valentine@gmail.com>
2021-02-04 19:51:52 -0300

    split front/back in two different repositories

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default App = () =>{
+    return(
+        <div>
+            <center>
+            <h1>Under development</h1>
+            <img src="./img/development.png" width="500"></img>
+            <div><a href="https://storyset.com/work">Illustration by Freepik Storyset</a></div>
+            </center>
+        </div>
+    )
+}
