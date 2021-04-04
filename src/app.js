4dd256adbc3b1a684df8fcef8b4f65d3432525aa
Mila <mila.valentine@gmail.com>
2021-04-04 16:17:39 -0300

    Trying to get it to compile properly

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 export default App = () =>{
     return(
-        <div>
-            <center>
-            <h1>Under development</h1>
-            <img src="./img/development.png" width="500"></img>
-            <div><a href="https://storyset.com/work">Illustration by Freepik Storyset</a></div>
-            </center>
-        </div>
-    )
+        <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
 }
