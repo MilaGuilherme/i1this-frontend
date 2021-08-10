8b9a1497e51894d35d64c243091f1de5cdf41008
Mila <mila.valentine@gmail.com>
2021-08-10 19:48:10 -0300

    Started home page

--- src/components/HotProducts.vue
+++ src/components/HotProducts.vue
@@ -1,28 +1,26 @@
 <template>
-<div class="grid grid-nogutter justify-content-around p-5" >
-  <template v-for="product in hotProducts" :key="product.index">
-    <Card class="col-2">
+  <h2 class="text-left">🔥 Hot products</h2>
+  <div class="hotproducts grid grid-nogutter">
+    <Card
+      v-for="product in hotProducts"
+      :key="product.index"
+      class="lg:col-2 md:col-3 sm:col-12 text-left m-2"
+    >
       <template #header>
-        <img :src="parse(product.photos)[0].src" style="height: 15rem" />
+        <img :src="product.photos[0].src" />
       </template>
       <template #title>
         {{ product.name }}
       </template>
-      <template #subtitle> Card subtitle </template>
-      <template #content>
-        <p>{{ product.description }}</p>
-      </template>
-      <template #footer>
-        <Button icon="pi pi-check" label="Save" />
-        <Button
-          icon="pi pi-times"
-          label="Cancel"
-          class="p-button-secondary"
-          style="margin-left: 0.5em"
+      <template #content
+        ><Tag
+          v-for="category in product.categories"
+          :key="category.index"
+          :value="category.name"
+          rounded
         />
       </template>
     </Card>
-  </template>
   </div>
 </template>
 
@@ -34,13 +32,10 @@ export default {
     return {};
   },
   methods: {
-    parse(string) {
-      return JSON.parse(string)
-    },
     async fetchProducts() {
-      const url = `${process.env.VUE_APP_API}/products?orderBy=ones`;
+      const url = `${process.env.VUE_APP_API}/products?order=ones&limit=6`;
       axios.get(url).then((response) => {
-        this.$store.dispatch("hotProducts", response.data.content);
+        this.$store.dispatch("setHotProducts", response.data.content);
       });
     },
   },
@@ -49,8 +44,16 @@ export default {
   },
   computed: {
     hotProducts() {
-      return this.$store.state.hotProducts;
+      return {data: this.$store.state.hotProducts};
     },
   },
 };
 </script>
+<style>
+.p-card .p-card-content {
+  padding: 0 !important;
+}
+.p-tag {
+  margin: 0.2em;
+}
+</style>
