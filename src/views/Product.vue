0c9169157675f96fdcacc12b3b24dcecd08e9bad
Mila <mila.valentine@gmail.com>
2021-08-11 16:41:25 -0300

    Product creation works

---
+++ src/views/Product.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    {{ product }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  Data() {
+    return {
+      product,
+    };
+  },
+  mounted: {
+    async product() {
+      const url = `${process.env.VUE_APP_API}${this.$route.path}`;
+      axios.get(url).then((response) => {
+        console.log(response)
+        product = response.data.content;
+      });
+    },
+  },
+};
+</script>
